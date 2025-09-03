cd871e557241015c04566a1acd5bed3db4388380
Hacker01russ <dehup182022@yandex.com>
2025-09-03 10:19:35 +0300

    newCommitLast

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,75 +1,50 @@
 package ru.netology.servlet;
-
 import ru.netology.controller.PostController;
-import ru.netology.exception.NotFoundException;
 import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
-
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
 public class MainServlet extends HttpServlet {
-
+  // контроллер, который будет управлять запросами и делегировать их в сервис
   private PostController controller;
-  private static final String API_POSTS = "/api/posts";
-  private static final String API_POSTS_ID = "/api/posts/\\d+";
-
   @Override
+// инициализация сервлета: создаем репозиторий, сервис и контроллер
   public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
-  }
+    final var repository = new PostRepository(); // создаем репозиторий
+    final var service = new PostService(repository); // создаем сервис
 
+    controller = new PostController(service); // создаем контроллер
+  }
   @Override
   protected void service(HttpServletRequest req, HttpServletResponse resp) {
-
     try {
       final var path = req.getRequestURI();
       final var method = req.getMethod();
-
-      if (method.equals("GET") && path.equals(API_POSTS)) {
-        controller.all(resp);
+      if (method.equals("GET") && path.equals("/api/posts")) {
+        controller.all(resp); // запрос для получения всех постов
         return;
       }
-
-      if (method.equals("GET") && path.matches(API_POSTS_ID)) {
-        try {
-          final var id = parseId(path);
-          controller.getById(id, resp);
-        } catch (NumberFormatException e) {
-          resp.setStatus(HttpServletResponse.SC_BAD_REQUEST); // Invalid ID format
-        } catch (NotFoundException e) {
-          resp.setStatus(HttpServletResponse.SC_NOT_FOUND); // Post not found
-        }
+      if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
+      // запрос для получения поста по id
+        final var id = Long.parseLong(path.substring(path.lastIndexOf("/"))); // извлекаем id из url
+        controller.getById(id, resp);
         return;
       }
-
-      if (method.equals("POST") && path.equals(API_POSTS)) {
-        controller.save(req.getReader(), resp);
+      if (method.equals("POST") && path.equals("/api/posts")) {
+        controller.save(req.getReader(), resp); // запрос на создание/обновление поста
         return;
       }
-
-      if (method.equals("DELETE") && path.matches(API_POSTS_ID)) {
-        try {
-          final var id = parseId(path);
-          controller.removeById(id, resp);
-        } catch (NumberFormatException e) {
-          resp.setStatus(HttpServletResponse.SC_BAD_REQUEST); // Invalid ID format
-        }
+      if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+        final var id = Long.parseLong(path.substring(path.lastIndexOf("/"))); // извлекаем id из url
+        controller.removeById(id, resp);
         return;
       }
-
       resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
     } catch (Exception e) {
-      e.printStackTrace();
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+      e.printStackTrace(); // печатаем ошибку в случае исключений
+      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR); // возвращаем ошибку
     }
-  }
-  private long parseId(String path) {
-    return Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
   }
 }
 
